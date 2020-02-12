4e329d214637b8ec550b2e0f7f701e3d61d66531
Rodrigo Donizetti de Oliveira <rodrigodonizettio@gmail.com>
2020-02-12 00:46:28 -0300

    Feature: Making firt GET request for ThingSpeak from frontEnd
    Further,
    - MainScreen function was changed to class type.
    - Using Async/Await
    - Using State to listen to the variables change and re-render the SPC.

--- mobile-app/BeerApp/src/pages/main.js
+++ mobile-app/BeerApp/src/pages/main.js
@@ -1,16 +1,35 @@
 import * as React from 'react';
-import { StyleSheet, SafeAreaView, ScrollView, View, Text } from 'react-native';
+import { StyleSheet, SafeAreaView, ScrollView, View, Text, Button } from 'react-native';
+import api from '../services/api';
 
-export function MainScreen() {
-    return (
-        <SafeAreaView>
-            <ScrollView contentInsetAdjustmentBehavior="automatic">
-                <View style={{ flex: 1, alignItems: 'center', justifyContent: 'center' }}>
-                    <Text>Main Screen</Text>
-                </View>
-            </ScrollView>
-        </SafeAreaView>
-    );
+export class MainScreen extends React.Component {
+    state = {
+        thingButtonValue: 0,
+    };
+
+    componentDidMount() {
+        this.loadThingSpeak();
+    }
+
+    loadThingSpeak = async () => {
+        const response = await api.get();
+        const { feeds } = response.data;
+        console.log(feeds[0].field1);
+        this.setState({ thingButtonValue: feeds[0].field1 })
+    };
+
+    render() {
+        return (
+            <SafeAreaView>
+                <ScrollView contentInsetAdjustmentBehavior="automatic">
+                    <View style={{ flex: 1, alignItems: 'center', justifyContent: 'center' }}>
+                        <Text>ThingSpeak value: {this.state.thingButtonValue}</Text>
+                        <Button title="Get ThingSpeak value!"></Button>
+                    </View>
+                </ScrollView>
+            </SafeAreaView>
+        );
+    }
 }
 
 const styles = StyleSheet.create({
